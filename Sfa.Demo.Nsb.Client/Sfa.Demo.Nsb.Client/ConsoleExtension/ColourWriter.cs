564a4846aabe0d92833a4043817e3108ebdca720
Joe Ingledew <joe.ingledew@sfa.bis.gov.uk>
2016-09-20 13:02:10 +0100

    Removed reference implementations

---
+++ Sfa.Demo.Nsb.Client/Sfa.Demo.Nsb.Client/ConsoleExtension/ColourWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace Sfa.Demo.Nsb.Client.ConsoleExtension
+{
+    using System;
+
+    public static class ColourWriter
+    {
+        private static readonly object MessageLock = new object();
+
+        public static void WriteLine(ConsoleColor color, string message, params object[] args)
+        {
+            lock (MessageLock)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message, args);
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- Sfa.Demo.Nsb.Client/Sfa.Demo.Nsb.Client/Program.cs
+++ Sfa.Demo.Nsb.Client/Sfa.Demo.Nsb.Client/Program.cs
@@ -4,8 +4,8 @@
 
     using NServiceBus;
 
+    using Sfa.Demo.Nsb.Client.ConsoleExtension;
     using Sfa.Demo.Nsb.Client.Helper;
-    using Sfa.Demo.Nsb.Common.Commands;
 
     public class Program
     {
@@ -22,9 +22,8 @@
 
             while (!exiting)
             {
-                Console.WriteLine("Select operation:");
-                Console.WriteLine("1 - Send an event");
-                Console.WriteLine("2 - Start listening for IDoSomething");
+                ColourWriter.WriteLine(ConsoleColor.Green, "Select operation:");
+                ColourWriter.WriteLine(ConsoleColor.Green, "1 - Send a command");
                 Console.WriteLine();
 
                 var userInput = Console.ReadLine();
@@ -32,21 +31,17 @@
                 switch (userInput)
                 {
                     case "1":
-                        SendAnEvent();
-                        exiting = PromptForExit();
-                        break;
-                    case "2":
-                        StartListeningForEvent();
+                        SendACommand();
                         exiting = PromptForExit();
                         break;
                     default:
-                        Console.WriteLine("Invalid input.");
+                        ColourWriter.WriteLine(ConsoleColor.Red, "Invalid input.");
                         exiting = PromptForExit();
                         break;
                 }
             }
 
-            Console.WriteLine("Press any key to exit...");
+            ColourWriter.WriteLine(ConsoleColor.Green, "Press any key to exit...");
             Console.ReadKey();
 
             BusConfigurator.Bus.Dispose();
@@ -54,29 +49,15 @@
 
         private static bool PromptForExit()
         {
-            Console.WriteLine("Exit? Y for yes, anything else for no");
+            ColourWriter.WriteLine(ConsoleColor.Green, "Exit? Y for yes, anything else for no");
             var exit = Console.ReadLine();
 
             return exit?.Trim().ToUpper() == "Y";
         }
 
-        private static void StartListeningForEvent()
-        {
-            Console.WriteLine("Waiting for event... Press any key to exit");
-
-            // your implementation here!
-            Console.ReadKey();
-        }
-
-        private static void SendAnEvent()
+        private static void SendACommand()
         {
             // Your impementation here!
-
-            // Demo implementation
-            Console.WriteLine("Input your name");
-            var name = Console.ReadLine();
-
-            BusConfigurator.Bus.Send<SayHello>("Demo.Server", cmd => { cmd.MyName = name; });            
         }
     }
 }
